8d336d27627158bb824043a7652c3d0d06f6f942
Atiqul <mailtoatiqul@gmail.com>
2024-05-04 18:18:38 +0600

    refactor readme file

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,135 +1,87 @@
-/*
-   Copyright 2022 Mohammad Faisal Khatri
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 const request = require('supertest');
 const expect = require('chai').expect;
-//const booking = require('../testdata/booking.json');
-//const userauthdata = require('../testdata/userauthdata.json');
-//const updatedbooking = require('../testdata/updatedbooking.json');
+const dotenv = require('dotenv');
+dotenv.config();
 
-describe('Restful Booker API Tests', () => {
-    const baseurl = 'http://localhost:3000';
-    
-    it('should successfully create a booking', (done) => {
+describe('Restfull File share API Tests', () => {
+    const baseurl = 'http://localhost:' + process.env.PORT;
+    let private_key = '';
+    let public_key = '';
+
+    it('should successfully create a ping', (done) => {
         request(baseurl)
             .get('/api/ping')
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json')
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(res.statusCode).to.be.equal(200);
-                expect(res.body).to.be.equal("Pong");
-                console.log(res.body)
+                expect(res.body).to.be.equal('Pong');
                 done();
             });
     });
 
-    // it('should fetch the booking of the provided booking id', (done) => {
-    //     request(baseurl)
-    //         .get('/booking/' + bookingId)
-    //         .set('Accept', 'application/json')
-    //         .set('Content-Type', 'application/json')
-    //         .end(function(err, res) {
-    //             expect(res.statusCode).to.be.equal(200);
-    //             expect(res.body.firstname).to.be.equal(booking.firstname);
-    //             expect(res.body.lastname).to.be.equal(booking.lastname);
-    //             expect(res.body.totalprice).to.be.equal(booking.totalprice);
-    //             expect(res.body.depositpaid).to.be.equal(booking.depositpaid);
-    //             expect(res.body.bookingdates.checkin).to.be.equal(booking.bookingdates.checkin);
-    //             expect(res.body.bookingdates.checkout).to.be.equal(booking.bookingdates.checkout);
-    //             expect(res.body.additionalneeds).to.be.equal(booking.additionalneeds);
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
+    it('should successfully upload file', (done) => {
+        const filePath = `${__dirname}/test.png`;
+        request(baseurl)
+            .post('/files')
+            .attach('file', filePath)
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(200);
+                private_key = res.body.payload.privateKey;
+                public_key = res.body.payload.publicKey;
+                done();
+            });
+    });
 
-    // it('should update the booking of the provided booking id using Put request', (done) => {
-    //     request(baseurl)
-    //         .put('/booking/' + bookingId)
-    //         .send(updatedbooking)
-    //         .set('Accept', 'application/json')
-    //         .set('Content-Type', 'application/json')
-    //         .set('Cookie', 'token=' + token)
-    //         .end(function(err, res) {
-    //             expect(res.statusCode).to.be.equal(200);
-    //             expect(res.body.firstname).to.be.equal(updatedbooking.firstname);
-    //             expect(res.body.lastname).to.be.equal(updatedbooking.lastname);
-    //             expect(res.body.totalprice).to.be.equal(updatedbooking.totalprice);
-    //             expect(res.body.depositpaid).to.be.equal(updatedbooking.depositpaid);
-    //             expect(res.body.bookingdates.checkin).to.be.equal(updatedbooking.bookingdates.checkin);
-    //             expect(res.body.bookingdates.checkout).to.be.equal(updatedbooking.bookingdates.checkout);
-    //             expect(res.body.additionalneeds).to.be.equal(updatedbooking.additionalneeds);
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
+    it('should successfully download file with public key', (done) => {
+        request(baseurl)
+            .get('/files/' + public_key)
+            .set('Content-Type', 'application/json')
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(200);
+                done();
+            });
+    });
 
-    // it('should update the firstname and lastname of booking of the provided booking id', (done) => {
-    //     var firstname = 'Michael';
-    //     var lastname = 'Trenor';
-    //     request(baseurl)
-    //         .patch('/booking/' + bookingId)
-    //         .send({ firstname: firstname, lastname: lastname })
-    //         .set('Accept', 'application/json')
-    //         .set('Content-Type', 'application/json')
-    //         .set('Cookie', 'token=' + token)
-    //         .end(function(err, res) {
-    //             expect(res.statusCode).to.be.equal(200);
-    //             expect(res.body.firstname).to.be.equal(firstname);
-    //             expect(res.body.lastname).to.be.equal(lastname);
-    //             expect(res.body.totalprice).to.be.equal(updatedbooking.totalprice);
-    //             expect(res.body.depositpaid).to.be.equal(updatedbooking.depositpaid);
-    //             expect(res.body.bookingdates.checkin).to.be.equal(updatedbooking.bookingdates.checkin);
-    //             expect(res.body.bookingdates.checkout).to.be.equal(updatedbooking.bookingdates.checkout);
-    //             expect(res.body.additionalneeds).to.be.equal(updatedbooking.additionalneeds);
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
+    it('should successfully delete file with private key', (done) => {
+        request(baseurl)
+            .delete('/files/' + private_key)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(200);
+                done();
+            });
+    });
 
-    // it('should Delete the booking of the provided booking id', (done) => {
-    //     request(baseurl)
-    //         .delete('/booking/' + bookingId)
-    //         .set('Accept', 'application/json')
-    //         .set('Content-Type', 'application/json')
-    //         .set('Cookie', 'token=' + token)
-    //         .end(function(err, res) {
-    //             expect(res.statusCode).to.be.equal(201);
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
-    // it('should show 404 status code for deleted booking id', (done) => {
-    //     request(baseurl)
-    //         .get('/booking/' + bookingId)
-    //         .set('Accept', 'application/json')
-    //         .set('Content-Type', 'application/json')
-    //         .end(function(err, res) {
-    //             expect(res.statusCode).to.be.equal(404);
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
+    it('should not Successfully delete while unknown private key', (done) => {
+        request(baseurl)
+            .delete('/files/' + '12121i12i1y2')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(400);
+                done();
+            });
+    });
+
+    it('should not successfully download file with unknown key', (done) => {
+        request(baseurl)
+            .get('/files/' + 'public_key')
+            .set('Content-Type', 'application/json')
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(400);
+                done();
+            });
+    });
+
+    it('should not successfully upload file without any file', (done) => {
+        const filePath = `${__dirname}/test.png`;
+        request(baseurl)
+            .post('/files')
+            .end(function (err, res) {
+                expect(res.statusCode).to.be.equal(500);
+                done();
+            });
+    });
 });
